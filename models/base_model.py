eb4b8bef89afb55e28bf5fd89f53088d473bf530
mazzinno <abdennasr3@gmail.com>
2023-12-10 19:01:38 +0100

    test

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ Write a class that defines all common attributes/methods for other classes
 """
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
@@ -32,8 +32,8 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -51,7 +51,7 @@ class BaseModel:
         updated_at with the current datetime
         """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """
@@ -61,4 +61,4 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         obj_dict['__class__'] = self.__class__.__name__
-        return obj_dict
+        return (obj_dict)
