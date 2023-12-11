a117c8d72be540ce70134bcadf324a2292836c3e
mazzinno <abdennasr3@gmail.com>
2023-12-11 02:03:46 +0100

    test

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 '''module base_model'''
+
+
 import uuid
 from datetime import datetime
 import models
 
 
 class BaseModel():
-    '''the baseModel class'''
+    '''baseModel class'''
     def __init__(self, *args, **kwargs):
-        '''class constructor for the class BaseModel'''
+        '''class constructor for class BaseModel'''
         if kwargs:
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -30,14 +32,12 @@ class BaseModel():
                                      self.id, self.__dict__)
 
     def save(self):
-        '''updates the public instance attribute
-        updated_at with the current datetime'''
+        '''updates 'updated_at' instance with current date/time'''
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        ''' returns a dictionary containing all
-        keys/values of __dict__ of the instance'''
+        '''dictionary representation ==> instance'''
         new_dict = dict(self.__dict__)
         new_dict['created_at'] = self.__dict__['created_at'].isoformat()
         new_dict['updated_at'] = self.__dict__['updated_at'].isoformat()
