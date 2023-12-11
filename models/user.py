c87e3fc5b29931544e403069576fd1a4865bb8de
mazzinno <abdennasr3@gmail.com>
2023-12-11 01:25:19 +0100

    done

--- models/user.py
+++ models/user.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-'''A class user'''
+""" Inherits from the BaseModel."""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    '''user class'''
-
+    """
+    Defines class User.
+    last_name (str): The last name of the user.
+    """
     email = ""
     password = ""
     first_name = ""
